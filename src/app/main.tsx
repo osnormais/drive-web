746aaa03fe5f4edce11e7081c4e52864ecaa0750
jhonatapers <jhonatapers@gmail.com>
2026-06-16 13:11:28 -0300

    feat: add global CSS styles and reset styles for consistent layout

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import './index.css'
-import "../styles/variables.css";
+import "../styles/globals.css";
 
 import App from './App.tsx'
 
